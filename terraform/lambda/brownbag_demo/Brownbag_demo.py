a8eb1aaa328e9a7e6a1078d42809389e0e3bf483
Leigh <philipleo.lerion@reedelsevier.com>
2021-11-17 09:21:40 +0800

    Save to S3

--- terraform/lambda/brownbag_demo/Brownbag_demo.py
+++ terraform/lambda/brownbag_demo/Brownbag_demo.py
@@ -1,11 +1,13 @@
-import configparser, json
+import configparser
+import os
 import urllib3
 import re
 import logging
-
 import collections
 import json
 import random
+import boto3
+from datetime import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,7 +16,16 @@ logger.setLevel(logging.INFO)
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
+# current date and time
+now = datetime.now()
+timestamp = str(int(datetime.now().timestamp()*1e3))
+
 http = urllib3.PoolManager()
+
+HTMLTAGPATTERNS = "copy-paste-block|div|<at>|</at>|&nbsp;|\n"
+
+bucketURL = config['S3_BUCKET']['bucket_url']
+bucketName = config['S3_BUCKET']['bucket_name']
 
 
 def sendMSTeamsNotification(message, trigger, status):
@@ -90,6 +101,36 @@ def sendMSTeamsNotification(message, trigger, status):
         logger.error("Failed Sending Notification - {}".format(error))
 
 
+def getAWSClient(clientService):
+    awsClient = None
+    try:
+        print("Establishing AWS {} Connection".format(clientService))
+        awsClient = boto3.client(clientService)
+    except AssertionError as error:
+        print(error)
+
+    return awsClient
+
+def paramParser(params):
+    patternTag = re.findall(HTMLTAGPATTERNS, params)
+
+    if patternTag:
+        if re.search("copy-paste-block", params):
+            start = params.find("copy-paste-block") + len("copy-paste-block") + 2
+            end = len(params)
+            substring = params[start:end]
+        else:
+            substring = params
+
+        for pat in patternTag:
+            substring = substring.replace(pat, "")
+    else:
+        logger.error("No match")
+        substring = params
+
+    return substring
+
+
 def rollDice(numberOfDice = 3, diceSideMax = 6, maxRoll = 100):
     diceSideMin = 1
     rollCount = 0
@@ -118,39 +159,149 @@ def rollDice(numberOfDice = 3, diceSideMax = 6, maxRoll = 100):
     return resultSumList
 
 
+def getRequest(requestMessage):
+    print("*** Retrieving Request Message ***")
+    print(requestMessage)
+
+    requestMessage = paramParser(requestMessage)
+
+    requestMessage = requestMessage.split("ToolTest")
+    request = requestMessage[1]
+
+    requestPayloadList = set()
+
+    if re.search(",", request):
+        print("Parameters Found")
+        payloadList = request.split(',')
+
+        for payload in payloadList:
+            payload = str(payload)
+            if re.search("&nbsp;", payload):
+                payload = payload.strip()
+
+            load = payload.strip()
+            if load is not None:
+                requestPayloadList.add(load)
+
+    print("*** REQUEST PAYLOAD LIST : {}".format(requestPayloadList))
+    return requestPayloadList
+
+
+def send2S3Bucket(s3Client, fileLocation):
+    success = True
+
+    try:
+        filepath = '/tmp/'
+        key = fileLocation[len(filepath):]
+
+        with open(fileLocation, 'rb') as data:
+            s3Client.upload_fileobj(data, bucketName, key)
+    except Exception as error:
+        success = False
+        print(error)
+
+    return success
+
+
+def save2FileFolder(dumpPayload):
+    # File Location and Naming
+    filename = config['FILE']['fileName']
+    fileExtension = config['FILE']['fileExtension']
+    fileLocation = None
+    fileFolderName = config['S3_BUCKET']['file_folder_name']
+
+    demoToolFileName = filename + timestamp + "_" + fileExtension
+    try:
+        filepath = '/tmp/'
+        folderName = filepath + fileFolderName
+
+        if not os.path.exists(folderName):
+            # create the folder
+            os.mkdir(folderName)
+
+        fileLocation = folderName + "/" + demoToolFileName
+
+        f = open(fileLocation, "w")
+        f.write(dumpPayload)
+        f.close()
+    except Exception as error:
+        print(error)
+
+    return fileLocation
+
+
 def handler(event, context):
+    print(" ****** EVENT : {}".format(event))
+
+    s3Client = getAWSClient("s3")
+
+    isMissingParams = False
+
     requestBody = event['body-json']
-    diceToRoll = event["number_of_dice"]
-    diceSideMax = event["sides_of_dice"]
-    maxRolledDice = event["roll_count"]
+    requestMessage = requestBody['text']
 
-    print(event)
+    try:
+        requestPayloadList = getRequest(requestMessage)
 
-    rolled = rollDice(diceToRoll, diceSideMax, maxRolledDice)
+        if requestPayloadList is None:
+            isMissingParams = True
+        else:
+            for requestPayload in requestPayloadList:
+                print(requestPayload)
+                req = requestPayload.split("=")
+                if re.search("number", req[0]):
+                    diceToRoll = int(req[1])
+                    print("diceToRoll = {}".format(diceToRoll))
+                elif re.search("sides", req[0]):
+                    diceSideMax = int(req[1])
+                    print("diceSideMax = {}".format(diceSideMax))
+                elif re.search("max", req[0]):
+                    maxRolledDice = int(req[1])
+                    print("maxRolledDice = {}".format(maxRolledDice))
 
-    responseList = []
-    for key in rolled.keys():
-        resultResponse = {
-            "sumOccurence": rolled[key],
-            "rolledDicesSum": key
-        }
-        responseList.append(resultResponse)
+            rolled = rollDice(diceToRoll, diceSideMax, maxRolledDice)
 
-    result = {
-        "NumberOfDices": diceToRoll,
-        "DiceRolledCount": maxRolledDice,
-        "SidesOfRolledDices": diceSideMax,
-        "DicesRolledResult": responseList,
-        "Raw": rolled
-    }
-    # requestMessage = requestBody['text']
-    # triggeredBy = requestBody['from']['name']
+            responseList = []
+            for key in rolled.keys():
+                resultResponse = {
+                    "sumOccurence": rolled[key],
+                    "rolledDicesSum": key
+                }
+                responseList.append(resultResponse)
 
-    #sendMSTeamsNotification(requestMessage, triggeredBy, "Successfull")
+            result = {
+                "NumberOfDices": diceToRoll,
+                "DiceRolledCount": maxRolledDice,
+                "SidesOfRolledDices": diceSideMax,
+                "DicesRolledResult": responseList,
+                "Raw": rolled
+            }
 
-    #print(requestMessage)
+            print(result)
 
-    return json.dumps({
-        'type': "String",
-        'text': result
-    })
+            result = json.dumps(result)
+
+            # Save the Result in S3
+            if s3Client is not None:
+                fileLocation = save2FileFolder(result)
+                print(fileLocation)
+
+                if fileLocation is not None:
+                    saved2S3 = send2S3Bucket(s3Client, fileLocation)
+                    print(saved2S3)
+
+    except Exception as error:
+        print(error)
+        isMissingParams = True
+        print("Exception Error, Parameters Missing - {}".format(requestMessage))
+
+    if bool(isMissingParams):
+        return json.dumps({
+            'type': "String",
+            'text': "Missing parameters invoking AWS Lambda Test Toll."
+        })
+    else:
+        return json.dumps({
+            'type': "String",
+            'text': result
+        })
